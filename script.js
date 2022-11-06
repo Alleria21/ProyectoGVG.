32ba01f58f339ef9030daa45f6de2b74707964b1
Rocio <roaguerrero17@gmail.com>
2022-11-05 22:02:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function contar1 () {
+    oferta1 = document.getElementById("oferta1").innerText;
+    oferta1--;
+    document.getElementById("oferta1").innerHTML = oferta1;
+}
+
+function contar2 () {
+    oferta2 = document.getElementById("oferta2").innerText;
+    oferta2--;
+    document.getElementById("oferta2").innerHTML = oferta2;
+}
+
+function contar3 () {
+    oferta3 = document.getElementById("oferta3").innerText;
+    oferta3--;
+    document.getElementById("oferta3").innerHTML = oferta3;
+}
+
+function contar4 () {
+    oferta4 = document.getElementById("oferta4").innerText;
+    oferta4--;
+    document.getElementById("oferta4").innerHTML = oferta4;
+}
+
+function contar5 () {
+    oferta5 = document.getElementById("oferta5").innerText;
+    oferta5--;
+    document.getElementById("oferta5").innerHTML = oferta5;
+}
+
+function contar6 () {
+    oferta6 = document.getElementById("oferta6").innerText;
+    oferta6--;
+    document.getElementById("oferta6").innerHTML = oferta6;
+}
